58f3875b4d124da3aee225df2c96f9b9fde2a47c
Daniele89sa2020 <daniele.calabro89@gmail.com>
2020-02-12 20:22:52 +0100

    Aggiunta funzionalità creazione attività offerta/richiesta

--- src/app/attivita-new-crea/attivita-new-crea.page.ts
+++ src/app/attivita-new-crea/attivita-new-crea.page.ts
@@ -1,4 +1,6 @@
-import { Component, OnInit } from '@angular/core';
+import { Component,Injectable, OnInit } from '@angular/core';
+import { ApiService } from '../api.service';
+import { AlertController } from '../../../node_modules/@ionic/angular';
 import { NavController } from '@ionic/angular';
 import {ActivatedRoute, NavigationExtras, Router} from '@angular/router';
 
@@ -11,19 +13,57 @@ export class AttivitaNewCreaPage implements OnInit {
 
   
   public currentUser: any=null;
+  public mess:String;
 
 
-  constructor(public navCtrl: NavController,
-              public route: ActivatedRoute) { }
+  constructor(
+    private apiService: ApiService,
+    public alertCtrl: AlertController,
+    public navCtrl: NavController,
+    private activatedRoute: ActivatedRoute,
+    private router: Router) {}
+
 
   ngOnInit() {
     this.currentUser=JSON.parse(localStorage.getItem('utente'));
   }
 
-  selectTipe(s){
-    //(<HTMLInputElement>document.getElementById('tipo')).value=s;
+  selectType(s){
+    (<HTMLInputElement>document.getElementById('tipo')).value=s;
   }
 
-  ionCreaAttivita(value,id){
+  ionCreaAttivita(value){
+    value.id_utente=this.currentUser.id;
+    this.apiService.creaAttivita(value).subscribe((data)=>{
+
+      if (data['status']=="no"){     
+                                 let messageNoLogin: any = {
+                                                     header: data['header'],
+                                                     message: data['message']
+                                                    };
+    
+                                  this.alertMex(messageNoLogin);                           
+                                }
+         else{ 
+              let messageNoLogin: any = {
+                                         header: data['header'],
+                                         message: data['message']
+                                        };
+
+              this.alertMex(messageNoLogin); 
+           }
+    });
   }
+
+
+  //ALERT
+async alertMex(forTask) {
+  const alert = await this.alertCtrl.create({
+      header: forTask.header,
+      message: forTask.message,
+      buttons: [{text: 'ok',},]
+  });
+  await alert.present();
+}
+
 }
